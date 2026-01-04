18c88f0efa1ab59c3494501f00c80ff2cef87b8c
Ghosting-x32 <1778942768@qq.com>
2026-01-04 16:26:02 +0800

    第9章

---
+++ 随堂软件/C++ Primer/第九章/316页_编写改变容器的循环程序/test_main.cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<iterator>
+#include<cstddef>
+#include<list>
+#include<deque>
+#include<forward_list>
+
+
+
+
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::string;
+using std::begin;
+using std::end;
+using std::size_t;
+using std::list;
+using std::deque;
+using std::forward_list;
+
+
+
+
+
+
+
+int main()
+{
+	vector<int> vi = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	auto iter = vi.begin();
+	while (iter != vi.end())
+	{
+		if (*iter % 2 == 1)
+		{
+			iter = vi.insert(iter, *iter);
+			iter += 2;
+		}
+		else
+		{
+			iter = vi.erase(iter);
+		}
+
+	}
+	for (const auto& c : vi)
+	{
+		cout << c << " ";
+	}
+
+	cout << endl;
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
---
+++ 随堂软件/C++ Primer/第九章/326页_指定在哪里开始搜索_string_find/test_main.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<iterator>
+#include<cstddef>
+#include<list>
+#include<deque>
+#include<forward_list>
+
+
+
+
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::string;
+using std::begin;
+using std::end;
+using std::size_t;
+using std::list;
+using std::deque;
+using std::forward_list;
+
+
+
+
+
+
+int main()
+{
+	string numbers("0123456789");
+	string name("5h6rjk2");
+
+	string::size_type pos = 0;
+	while ((pos = name.find_first_of(numbers, pos)) != string::npos)
+	{
+		cout << "found number at index: " << pos << " elemennt is " << name[pos] << endl;
+		++pos;
+	}
+
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
